8577018ef0ebe2b73c9c25cac1ebef11832b0d2b
WooLegend <dnwogus4260@naver.com>
2024-10-21 17:15:51 +0900

    docs: 비동기 처리 커스텀 훅 JSDOC 추가

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,6 +1,12 @@
 import { useCallback } from "react";
 import { useState } from "react";
 
+/**
+ * API의 비동기 처리를 돕는 커스텀 훅
+ * @param {function} asyncFunction 랩핑할 API 함수
+ * @returns {[pending: boolean, error : object, wrappedFunction : function]}
+ * @todo 반환 타입 object로 변경하기
+ */
 function useAsync(asyncFunction) {
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(null);
