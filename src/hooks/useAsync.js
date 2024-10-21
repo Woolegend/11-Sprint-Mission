9a20d8084a9da3847fe98785b477dee9a80cbaf5
WooLegend <dnwogus4260@naver.com>
2024-10-22 01:55:44 +0900

    feat: useAsync 개선

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -4,18 +4,17 @@ import { useState } from "react";
 /**
  * API의 비동기 처리를 돕는 커스텀 훅
  * @param {function} asyncFunction 랩핑할 API 함수
- * @returns {[pending: boolean, error : object, wrappedFunction : function]}
- * @todo 반환 타입 object로 변경하기
+ * @returns {{pending: boolean, error : object, execute : function}}
  */
 function useAsync(asyncFunction) {
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(null);
 
-  const wrappedFunction = useCallback(
+  const execute = useCallback(
     async (...args) => {
       try {
-        setError(null);
         setPending(true);
+        setError(null);
         return await asyncFunction(...args);
       } catch (err) {
         setError(err);
@@ -27,7 +26,7 @@ function useAsync(asyncFunction) {
     [asyncFunction]
   );
 
-  return [pending, error, wrappedFunction];
+  return { pending, error, execute };
 }
 
 export default useAsync;
